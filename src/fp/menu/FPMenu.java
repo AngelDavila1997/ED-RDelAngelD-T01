4d20f44a690b2e2c87055f6ab0acbf71ea3bf1de
PC <PC@HP>
2016-10-03 12:21:35 -0500

    FPMenu. Muestra de menus consola en JAVA

--- src/fp/menu/FPMenu.java
+++ src/fp/menu/FPMenu.java
@@ -19,17 +19,45 @@ public class FPMenu {
        
         //Metodos
         System.out.print("La opcion elegida es:" + menu());
+        
     }
     public static int menu(){
         int opcion;
         Scanner teclado = new Scanner(System.in);
+       do{
         System.out.print("Calculo de areas");
-        System.out.print("Menù:");
+        System.out.print("Menu:");
         System.out.print("Introduce la opciòn deseada");
         System.out.print("1. Area de cuadrados");
         System.out.print("2. Area de circulos");
         System.out.print("3. Area de triangulos");
+        System.out.print("0. Salir");
         opcion=teclado.nextInt();
+         }while (opcion <0 || opcion >= 4);
         return opcion;
+    }
+    public static float calculaAreas(int a){
+      float area;
+        switch(a){
+            case 1:
+                area = areaCuadrado();
+                break;
+            case 2:
+                area = areaCirculo();
+                break;
+            case 3:
+                area = areaTriangulo();
+                break;
+            default:
+                area = mensajeSalida();
+                break;
+        }
+        return area;
+    }
+    public static int areaCuadrado(int a){
+        return Math.pow(a,2);
+    }
+    public static int areaCirculo(int a){
+        return 
     }
 }
