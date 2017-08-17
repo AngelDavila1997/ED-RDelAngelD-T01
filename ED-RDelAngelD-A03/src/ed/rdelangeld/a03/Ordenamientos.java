db2edfcc947dc5bf8bb99090b6391ea834e7a0c7
Ricardo Ángel Del Ángel Dávila <A01411440@itesm.mx>
2017-08-17 14:38:26 -0500

    Algoritmos de busqueda

---
+++ ED-RDelAngelD-A03/src/ed/rdelangeld/a03/Ordenamientos.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ed.rdelangeld.a03;
+
+/**
+ *
+ * @author A01411440
+ */
+public interface Ordenamientos {
+    void seleccion();
+}
